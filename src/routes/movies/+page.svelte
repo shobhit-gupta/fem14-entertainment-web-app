47d437781f282807b517b99df32ba667d25722ec
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-06-04 08:09:52 +0000

    fix: Remove unintended scroll on pages

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,3 +1,3 @@
-<div class="m-6 flex min-h-screen items-center justify-center text-center text-3xl text-white">
+<div class="mx-6 flex min-h-screen items-center justify-center text-center text-3xl text-white">
 	Movies
 </div>
