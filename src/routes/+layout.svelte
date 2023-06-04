2db0bbde2e86ac5b2466030824493126ab0f1bcc
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-06-04 06:09:12 +0000

    refactor: Abstract out css classes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,28 +16,7 @@
 	<title>Entertainment web app</title>
 </svelte:head>
 
-<nav
-	class="
-        fixed
-        inset-x-0
-        top-0
-        flex
-        items-center
-        justify-between
-        bg-semiDarkBlue
-        p-4
-        md:inset-x-6
-        md:top-6
-        md:rounded-[10px]
-        md:p-6
-        lg:inset-y-8
-        lg:left-8
-        lg:right-auto
-        lg:flex-col
-        lg:rounded-[20px]
-        lg:p-8
-    "
->
+<nav>
 	<img src="/assets/logo.svg" alt="" class="nav__logo" />
 	<div class="flex gap-6 md:gap-8 lg:basis-3/4 lg:flex-col lg:gap-10">
 		<img src="/assets/icon-nav-home.svg" alt="" class="nav__icon" />
@@ -45,7 +24,7 @@
 		<img src="/assets/icon-nav-tv-series.svg" alt="" class="nav__icon" />
 		<img src="/assets/icon-nav-bookmark.svg" alt="" class="nav__icon" />
 	</div>
-	<div class="h-6 w-6 rounded-full ring-1 ring-white md:h-8 md:w-8 lg:h-10 lg:w-10">
+	<div class="nav__avatar">
 		<img src="/assets/image-avatar.png" alt="" />
 	</div>
 </nav>
@@ -53,10 +32,44 @@
 <slot />
 
 <style lang="postcss">
+	nav {
+		@apply fixed
+            inset-x-0
+            top-0
+            flex
+            items-center
+            justify-between
+            bg-semiDarkBlue
+            p-4
+            md:inset-x-6
+            md:top-6
+            md:rounded-[10px]
+            md:p-6
+            lg:inset-y-8
+            lg:left-8
+            lg:right-auto
+            lg:flex-col
+            lg:rounded-[20px]
+            lg:p-8;
+	}
+
 	.nav__logo {
 		@apply h-5 w-auto md:h-auto md:w-8;
 	}
+
 	.nav__icon {
 		@apply h-4 w-4 md:h-5 md:w-5;
+	}
+
+	.nav__avatar {
+		@apply h-6
+            w-6
+            rounded-full
+            ring-1
+            ring-white
+            md:h-8
+            md:w-8
+            lg:h-10
+            lg:w-10;
 	}
 </style>
