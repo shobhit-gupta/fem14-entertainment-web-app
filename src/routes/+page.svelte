8f0cea02d7991d6f31d6d4412ce8555ab81251c6
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-06-07 14:48:37 +0000

    feat: Add cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,43 @@
 <div
-	class="flex min-h-full items-center justify-center gap-12 bg-red text-center text-3xl text-white"
+	class="flex min-h-full flex-col items-center justify-center gap-12 bg-red text-center text-3xl text-white"
 >
-	Home
+	<p>Home</p>
+	<article class="body-s grid w-72 grid-cols-1 space-y-2 text-left leading-tight">
+		<div class="overflow-hidden rounded-lg">
+			<img src="/assets/thumbnails/beyond-earth/regular/large.jpg" alt="" />
+		</div>
+		<div class="flex flex-col gap-1">
+			<div class="flex gap-2 opacity-75">
+				<p>2019</p>
+				<p>&sdot;</p>
+				<div class="flex items-center space-x-1.5">
+					<img src="/assets/icon-category-movie.svg" alt="" />
+					<p>Movie</p>
+				</div>
+				<p>&sdot;</p>
+				<p>PG</p>
+			</div>
+			<h4 class="display-xs">Beyond Earth</h4>
+		</div>
+	</article>
+	<article class="body-m grid w-[470px] grid-cols-1 space-y-2 text-left leading-tight">
+		<div class="col-start-1 row-span-2 row-start-1 overflow-hidden rounded-lg">
+			<img src="/assets/thumbnails/beyond-earth/trending/large.jpg" alt="" />
+		</div>
+		<div
+			class="col-start-1 row-start-2 flex flex-col gap-1 rounded-b-lg bg-gradient-to-b from-transparent to-black/75 p-6"
+		>
+			<div class="flex gap-2 opacity-75">
+				<p>2019</p>
+				<p>&sdot;</p>
+				<div class="flex items-center space-x-1.5">
+					<img src="/assets/icon-category-movie.svg" alt="" />
+					<p>Movie</p>
+				</div>
+				<p>&sdot;</p>
+				<p>PG</p>
+			</div>
+			<h4 class="display-sm">Beyond Earth</h4>
+		</div>
+	</article>
 </div>
