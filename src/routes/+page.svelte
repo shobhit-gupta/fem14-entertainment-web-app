509b58fa24083d44e531a6292d0ee474241c7348
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-06-08 01:08:11 +0000

    feat: Add play button on hover

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <div
-	class="flex min-h-full flex-col items-center justify-center gap-12 bg-red text-center text-3xl text-white"
+	class="flex min-h-full flex-col items-center justify-center gap-12 bg-red text-center text-white"
 >
-	<p>Home</p>
+	<p class="text-3xl">Home</p>
 
-	<article class="body-sm card w-72">
-		<div class="card__img-wrapper">
+	<article class="body-sm card group w-72">
+		<div class="card__img-wrapper relative">
 			<picture>
 				<source
 					media="(max-width: 767px)"
@@ -16,8 +16,18 @@
 				/>
 				<img src="/assets/thumbnails/beyond-earth/regular/large.jpg" alt="" />
 			</picture>
+			<a href="/">
+				<div
+					class="absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+				>
+					<div class="flex items-center gap-5 rounded-full bg-white/25 p-[9px] pr-6">
+						<img src="/assets/icon-play.svg" alt="" />
+						<span class="display-xs">Play</span>
+					</div>
+				</div>
+			</a>
 		</div>
-		<div class="card__info-container">
+		<div class="card__info-container relative">
 			<div class="flex gap-2 opacity-75">
 				<p>2019</p>
 				<p>&sdot;</p>
@@ -32,8 +42,8 @@
 		</div>
 	</article>
 
-	<article class="body-md trending card w-[470px]">
-		<div class="card__img-wrapper">
+	<article class="body-md trending card group w-[470px]">
+		<div class="card__img-wrapper relative">
 			<picture>
 				<source
 					media="(max-width: 767px)"
@@ -41,8 +51,18 @@
 				/>
 				<img src="/assets/thumbnails/beyond-earth/trending/large.jpg" alt="" />
 			</picture>
+			<a href="/">
+				<div
+					class="absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+				>
+					<div class="flex items-center gap-5 rounded-full bg-white/25 p-[9px] pr-6">
+						<img src="/assets/icon-play.svg" alt="" />
+						<span class="display-xs">Play</span>
+					</div>
+				</div>
+			</a>
 		</div>
-		<div class="card__info-container">
+		<div class="card__info-container relative">
 			<div class="flex gap-2 opacity-75">
 				<p>2019</p>
 				<p>&sdot;</p>
@@ -60,7 +80,7 @@
 
 <style lang="postcss">
 	.card {
-		@apply grid grid-cols-1 space-y-2 text-left leading-tight;
+		@apply grid grid-cols-1 space-y-2 text-left;
 	}
 	.card__img-wrapper {
 		@apply overflow-hidden rounded-lg;
